11fae32a33e37d33547373e16e8ab1de346f6917
Jorge Baez <jibm_82@yahoo.com.mx>
2019-04-27 01:01:41 -0500

    Added name to user

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,12 @@
+# frozen_string_literal: true
+
 class ApplicationController < ActionController::Base
+  before_action :configured_permitted_parameters, if: :devise_controller?
+end
+
+protected
+
+def configured_permitted_parameters
+  devise_parameter_sanitizer.permit(:account_update, keys: %i(name))
+  devise_parameter_sanitizer.permit(:sign_up, keys: %i(name))
 end
