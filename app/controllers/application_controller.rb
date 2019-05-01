e9253410fbcab5b0de6a4248869476823051da2c
Jorge Baez <jibm_82@yahoo.com.mx>
2019-05-01 01:06:18 -0500

    Implemented styles for login page

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,11 +2,11 @@
 
 class ApplicationController < ActionController::Base
   before_action :configured_permitted_parameters, if: :devise_controller?
-end
 
-protected
+  protected
 
-def configured_permitted_parameters
-  devise_parameter_sanitizer.permit(:account_update, keys: %i(name))
-  devise_parameter_sanitizer.permit(:sign_up, keys: %i(name))
+  def configured_permitted_parameters
+    devise_parameter_sanitizer.permit(:account_update, keys: %i(name))
+    devise_parameter_sanitizer.permit(:sign_up, keys: %i(name))
+  end
 end
